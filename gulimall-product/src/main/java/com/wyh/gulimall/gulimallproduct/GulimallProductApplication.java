eed64ac731d45d6fff30d976baa85187fe1bf1e0
wyh97210 <2568984782@qq.com>
2020-07-18 12:17:11 +0800

    项目初始结构创建

---
+++ gulimall-product/src/main/java/com/wyh/gulimall/gulimallproduct/GulimallProductApplication.java
@@ -0,0 +1,13 @@
+package com.wyh.gulimall.gulimallproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallProductApplication.class, args);
+    }
+
+}
